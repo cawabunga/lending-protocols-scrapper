f2bf1c389b907f0e328dc816ec2367825dd2f842
Emil Ibatullin <emil.ibatullin@gmail.com>
2023-01-25 21:26:49 +0300

    Add filtering by token

--- docs/src/index.js
+++ docs/src/index.js
@@ -1,81 +1,124 @@
-function main() {
-	const root = document.querySelector('#root');
-	const app = document.createElement(AppComponent.tagName);
-	root.appendChild(app);
+import {
+  LitElement,
+  html,
+} from "https://cdn.jsdelivr.net/gh/lit/dist@2/core/lit-core.min.js";
+
+const stables = [
+  "USDT",
+  "DAI",
+  "USDC",
+  "sUSD",
+  "USDT.e",
+  "USDC.e",
+  "DAI.e",
+  "FUSDT",
+];
+
+class AppRoot extends LitElement {
+  static properties = {
+    aprs: {},
+    query: {},
+    onlyStables: {},
+  };
+
+  constructor() {
+    super();
+    this.aprs = [];
+    this.query = "";
+    this.onlyStables = true;
+  }
+
+  async connectedCallback() {
+    await super.connectedCallback();
+
+    this.aprs = [await loadAave(), await loadIronBank()].flat();
+  }
+
+  render() {
+    return html` <p>
+        <label>
+          Filter by token:
+          <input
+            type="text"
+            placeholder="usd"
+            .value="${this.query}"
+            @input=${this._handleQueryChange}
+          />
+        </label>
+
+        <label>
+          <input
+            type="checkbox"
+            .checked="${this.onlyStables}"
+            @click="${this._handleStableSwitch}"
+          />
+          Only Stablecoins
+        </label>
+      </p>
+
+      <table>
+        <thead>
+          <th>Chain</th>
+          <th>Protocol</th>
+          <th>Symbol</th>
+          <th>APY</th>
+        </thead>
+        <tbody>
+          ${this.aprs
+            .sort((a, b) => sum(b.apy) - sum(a.apy))
+            .filter((apr) =>
+              this.onlyStables ? stables.includes(apr.tokenSymbol) : true
+            )
+            .filter((apr) =>
+              apr.tokenSymbol.toLowerCase().includes(this.query.toLowerCase())
+            )
+            .map(
+              (apr) =>
+                html`
+                  <tr>
+                    <td>${apr.chainName}</td>
+                    <td>${apr.protocol}</td>
+                    <td>${apr.tokenSymbol}</td>
+                    <td
+                      style="font-variant-numeric: tabular-nums; text-align: center;"
+                    >
+                      ${computeTotal(apr.apy)}
+                    </td>
+                  </tr>
+                `
+            )}
+        </tbody>
+      </table>`;
+  }
+
+  _handleQueryChange = (event) => {
+    this.query = event.target.value;
+  };
+
+  _handleStableSwitch = (event) => {
+    this.onlyStables = event.target.checked;
+  };
 }
 
-export class AppComponent extends HTMLElement {
-	static tagName = 'app-component';
+customElements.define("app-root", AppRoot);
 
-	async connectedCallback() {
-		this.render();
-
-		const aprs = [
-			await loadAave(),
-			await loadIronBank()
-		].flat()
-
-		this.renderAprs(aprs)
-	}
-
-	render() {
-		this.innerHTML = `<table>
-	<thead>
-		<th>Chain</th>
-		<th>Protocol</th>
-		<th>Symbol</th>
-		<th>APY</th>
-	</thead>
-	<tbody class="js-table-body"></tbody>
-</table>`;
-	}
-
-	renderAprs(aprs) {
-		const table = this.querySelector('.js-table-body');
-
-		aprs
-			.map((aprItem) => {
-				const tr = document.createElement('tr');
-
-				[
-					makeTd(aprItem.chainName),
-					makeTd(aprItem.protocol),
-					makeTd(aprItem.tokenSymbol),
-					makeApyTd(aprItem.apy),
-				].forEach((node) => {
-					tr.append(node)
-				})
-
-				return tr;
-			})
-			.forEach(tr => {
-				table.appendChild(tr);
-			});
-	}
-}
-
-customElements.define(AppComponent.tagName, AppComponent);
-
-function makeTd(textContent) {
-	const td = document.createElement('td');
-	td.innerText = textContent;
-	return td
-}
-
-function makeApyTd(apy) {
-	const MULTIPLIER = 10000
-	const totalApy = apy.map((a) => a * MULTIPLIER).reduce((a, b) => a + b, 0) / MULTIPLIER;
-	return makeTd(`${totalApy}%`)
+function computeTotal(apy) {
+  const MULTIPLIER = 10000;
+  const totalApy =
+    apy.map((a) => a * MULTIPLIER).reduce((a, b) => a + b, 0) / MULTIPLIER;
+  return `${totalApy}%`;
 }
 
 async function loadAave() {
-	const response = await fetch('./files/aave.json');
-	return response.json();
+  const response = await fetch("./files/aave.json");
+  return response.json();
 }
 
 async function loadIronBank() {
-	const response = await fetch('./files/iron-bank.json');
-	return response.json();
+  const response = await fetch("./files/iron-bank.json");
+  return response.json();
 }
 
-main();
+function sum(array) {
+  return array.reduce((a, b) => a + b, 0);
+}
